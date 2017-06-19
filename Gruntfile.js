d5611d9d671c1a2fa474f61e373ea42d98fd8f82
Roman Stepanov <romik112@yandex.ru>
2017-06-19 11:23:41 +0300

    Создал Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    clean: {
+      build: 'build/'
+    },
+
+    copy: {
+      html: {
+        files: [{
+          expand: true,
+          src: '*.html',
+          dest: 'build/'
+        }]
+      }
+    }
+  });
+
+  grunt.registerTask('build', [
+    'clean',
+    'copy:html'
+  ]);
+};
