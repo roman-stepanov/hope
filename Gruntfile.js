94c33825572db71a8c07862865ebf00596ea90ff
Roman Stepanov <romik112@yandex.ru>
2017-06-19 11:42:01 +0300

    Добавил normalize.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,14 @@ module.exports = function(grunt) {
           src: '*.html',
           dest: 'build/'
         }]
+      },
+      normalize: {
+        files: [{
+          expand: true,
+          cwd: 'node_modules/normalize.css/',
+          src: 'normalize.css',
+          dest: 'build/css/'
+        }]
       }
     },
 
@@ -46,7 +54,8 @@ module.exports = function(grunt) {
 
   grunt.registerTask('build', [
     'clean',
-    'copy:html'
+    'copy:html',
+    'copy:normalize'
   ]);
 
   grunt.registerTask('serve', [
