a107d465c92457148aca487299d8a66249c6ecef
Roman Stepanov <romik112@yandex.ru>
2017-06-21 13:42:45 +0300

    Сделал переключение фильтра портфолио

--- js/common.js
+++ js/common.js
@@ -1,12 +1,37 @@
 $(document).ready(function() {
+  var portfolioFilter = $('#portfolio-filter');
+  var portfolioActiveFilter = portfolioFilter.find('.portfolio__filter-link--active');
+  var portfolioGrid = $('#portfolio-grid');
 
-  var portfolioGridContainer = $('#portfolio-grid-container');
+  var updatePortfolioGrid = function() {
+    portfolioGrid.animate(
+      {opacity: 0}, 500,
+      function() {
+        portfolioGrid.animate(
+          {opacity: 1}, 500
+        );
+      }
+    );
+  };
 
-  portfolioGridContainer.imagesLoaded(function() {
-    portfolioGridContainer.masonry({
+  var onClickPortfolioFilter = function(evt) {
+    evt.preventDefault();
+    var target = $(this);
+
+    portfolioActiveFilter.removeClass('portfolio__filter-link--active');
+    target.addClass('portfolio__filter-link--active');
+    portfolioActiveFilter = target;
+
+    updatePortfolioGrid();
+  };
+
+  portfolioGrid.imagesLoaded(function() {
+    portfolioGrid.masonry({
       itemSelector: '.portfolio__grid-item',
       columnWidth: 370,
       gutter: 30
     });
   });
+
+  portfolioFilter.find('a').on('click', onClickPortfolioFilter);
 });
